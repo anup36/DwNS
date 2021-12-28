2ad44c4cfa1f14af073dd70dec27373eecb2c3d8
anuppanwar36 <anuppanwar36@gmail.com>
2021-12-28 17:16:25 +0530

    added validation for preventing multiple minting

--- contracts/DwNS.sol
+++ contracts/DwNS.sol
@@ -1,15 +1,20 @@
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >= 0.6.0 <= 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BNS is ERC1155{
     uint public nft_ids;
-    uint public token_ids;
+    uint public token_ids = 1000;
     uint public token_sold;
     address public owner;
+    uint totalSupply = 100000;
+    // string name;
+    // string symbol;
 
-    constructor() ERC1155("Iotric") {
+
+    constructor() ERC1155("") {
         owner = msg.sender;
     }
 
@@ -21,7 +26,6 @@ contract BNS is ERC1155{
     struct nft_detail{
         address owner;
         string name;
-        string types;
     }
 
     struct tlds{
@@ -31,46 +35,54 @@ contract BNS is ERC1155{
         string url;
         bool readyToBeSold;
         uint price;
-        string types;
     }
-    mapping(uint => nft_detail) public Zones;
-    mapping(uint => tlds) public TLDs;
-    mapping(address => uint) public balances;
 
-    function createZone(string memory name) onlyOwner public {
-        for(uint i = 0; i < nft_ids; i++){
-            require(keccak256(abi.encodePacked(name)) != keccak256(abi.encodePacked(Zones[i].name)), "Domain Already Exists!");
+
+    mapping(uint => nft_detail) public Zones;
+    
+    mapping(uint => tlds) public TLDs;
+
+    function createZone(string memory _name) onlyOwner public {
+        // name = _name;
+        // symbol = _symbol;
+
+        for(uint i = 0; i <= nft_ids; i++){
+            require(keccak256(abi.encodePacked(_name)) != keccak256(abi.encodePacked(Zones[i].name)), "Zone Already Exists!");
         }
         nft_ids = nft_ids + 1;
 
-        Zones[nft_ids] = nft_detail(msg.sender, name);
+        Zones[nft_ids] = nft_detail(msg.sender, _name);
 
         _mint(msg.sender, nft_ids, 1, "");
-        balances[msg.sender] = 1;
+
         //NFT Created!
     }
 
-    function createTld(string memory label, uint parentId, string memory uri, uint totalSupply) public {
-        for(uint i = 0; i < token_ids; i++){
-            require(keccak256(abi.encodePacked(label)) != keccak256(abi.encodePacked(TLDs[i].label)));
+    function createTld(string memory label, uint parentId, string memory uri) public {
+        for(uint i = 1000; i <= token_ids; i++){
+            require(keccak256(abi.encodePacked(label)) != keccak256(abi.encodePacked(TLDs[i].label)), "Domain/Subdomain Already Exists!");
         }
+        // token_ids = uint(keccak256(abi.encodePacked(block.timestamp + 5))) % 10000000000000;
         token_ids = token_ids + 1;
 
-    TLDs[token_ids] = tlds(msg.sender, label, parentId, uri, false, 0);
+        TLDs[token_ids] = tlds(msg.sender, label, parentId, uri, false, 0);
 
-    _mint(msg.sender, token_ids, totalSupply, bytes(uri));
-    
-    balances[msg.sender] = totalSupply;
+        _mint(msg.sender, token_ids, 1, bytes(uri));
+
     //New Domain and Label Created!
     }
 
-    function balanceOf(address user, uint amount, string memory types, uint tokenId) public {
-        if (types == Zones[tokenId].types) {
-            return balances[user];
-        }
-        else if(types == TLDs[tokenId].types) {
-            return balances[user];
-        }
+    function issueIco(uint tokenId, uint _totalSupply) public {
+        _mint(msg.sender, tokenId, _totalSupply, "");
+
+        reserveForAdmin(tokenId);
+    }
+
+    function reserveForAdmin(uint tokenId) public {
+        // reserveForAdmin(token_ids);
+        tokenId = tokenId + 2;
+        uint iotricShare = (totalSupply * 10)/ 100;
+        safeTransferFrom(msg.sender, owner, tokenId, iotricShare, "");
     }
 
     function sellTld(uint tokenId, uint price) public {
@@ -86,4 +98,5 @@ contract BNS is ERC1155{
         safeTransferFrom(address(0), msg.sender, tokenId, 1, "");
         token_sold = token_sold + 1;
     }
+
 }
